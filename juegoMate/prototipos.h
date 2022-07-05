4b8d1c5a409c7d8b8fe3e8fa951afb0652198bfe
ChorchPig <96993431+ChorchPig@users.noreply.github.com>
2022-07-05 14:05:22 -0300

    Add files via upload

---
+++ juegoMate/prototipos.h
@@ -0,0 +1,9 @@
+#ifndef PROTOTIPOS_H_INCLUDED
+#define PROTOTIPOS_H_INCLUDED
+
+void confirmarSalida(void *);
+void juegoAlgebra(int *);
+void pedirResultado(int, int, int, int *);
+void juegoGeometria(int *);
+
+#endif // PROTOTIPOS_H_INCLUDED
